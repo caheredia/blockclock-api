080d42cce043623980bb0d9545e4f3f2c8faa68e
Cristian Heredia <cristianheredia@protonmail.com>
2022-12-02 23:34:04 -0800

    Installed reqwest, futures, and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let result = reqwest::get("http://192.168.10.196/api/show/text/Wow").await;
+    println!("{:#?}", result)
 }

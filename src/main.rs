2f8491901ab287fc09f98586964cf6edf6c76630
Cristian Heredia <cristianheredia@protonmail.com>
2022-12-03 18:06:39 -0800

    Changed text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #[tokio::main]
 async fn main() {
-    let result = reqwest::get("http://192.168.10.196/api/show/text/Wow").await;
+    let result = reqwest::get("http://192.168.10.196/api/show/text/HiBaby").await;
     println!("{:#?}", result)
 }
